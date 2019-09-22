ba3e0e461df231079da78932b500c21515bf712b
lisaac <lisaac.cn@gmail.com>
2019-09-22 20:54:54 +0800

    update for logs

--- docker.lua
+++ docker.lua
@@ -2,6 +2,7 @@ require "nixio.util"
 local json = require "luci.json"
 local nixio = require "nixio"
 local http = require "luci.http.protocol"
+local ltn12 = require "luci.ltn12"
 --[[
 
   QUICK START:
@@ -16,6 +17,70 @@ local http = require "luci.http.protocol"
   https://docs.docker.com/engine/api
 
 ]]
+
+
+local chunksource = function(sock, buffer)
+	buffer = buffer or ""
+	return function()
+    local output
+    --方法存在bug，需要修补
+		local _, endp, count = buffer:find("^([0-9a-fA-F]+);?.-\r\n")
+		while not count and #buffer <= 1024 do
+			local newblock, code = sock:recv(1024 - #buffer)
+			if not newblock then
+				return nil, code
+			end
+			buffer = buffer .. newblock  
+			_, endp, count = buffer:find("^([0-9a-fA-F]+);?.-\r\n")
+		end
+		count = tonumber(count, 16)
+		if not count then
+			return nil, -1, "invalid encoding"
+		elseif count == 0 then
+			-- 最后一行，结束
+			return nil
+		elseif count + 2 <= #buffer - endp then
+			-- 当前不是最后一行，需要pumps继续处理，所以不要忘记buffer部分的处理
+			output = buffer:sub(endp+1, endp+count)
+			buffer = buffer:sub(endp+count+3)
+			return output
+		else
+			-- buffer中的数据比count中的数据要少，需要从sock读取
+			output = buffer:sub(endp+1, endp+count)
+			buffer = ""
+			if count - #output > 0 then
+				local remain, code = sock:recvall(count-#output)
+				if not remain then
+					return nil, code
+				end
+				output = output .. remain
+				count, code = sock:recvall(2)
+			else
+				count, code = sock:recvall(count+2-#buffer+endp)
+			end
+			if not count then
+				return nil, code
+			end
+			return output
+		end
+	end
+end
+
+local docker_stream_filter = function(buffer)
+  local output = ""
+  buffer = buffer or ""
+  if #buffer < 8 then
+    return ""
+  end
+  local stream_type = ((string.byte(buffer, 1) == 1) and "stdout") or ((string.byte(buffer, 1) == 2) and "stderr") or ((string.byte(buffer, 1) == 0) and "stdin") or "stream_err"
+  local valid_length =
+    tonumber(string.byte(buffer, 5)) * 256 * 256 * 256 + tonumber(string.byte(buffer, 6)) * 256 * 256 + tonumber(string.byte(buffer, 7)) * 256 + tonumber(string.byte(buffer, 8))
+  if valid_length > #buffer + 8 then
+    return ""
+  end
+  return stream_type .. ": " .. string.sub(buffer, 9, valid_length + 8)
+end
+
 local gen_http_req = function(options)
   local req
   options = options or {}
@@ -41,9 +106,48 @@ local send_http_socket = function(socket_path, req)
   if docker_socket:send(req) == 0 then
     return 'HTTP/1.1 400 send socket error\r\n{"message":"can\'t send data to unix socket"}'
   end
-  local data, err_code, err_msg, data_f = docker_socket:readall()
+  -- local data, err_code, err_msg, data_f = docker_socket:readall()
+  -- docker_socket:close()
+  local linesrc = docker_socket:linesource() -- 读取 socket 将用 source http://w3.impa.br/~diego/software/luasocket/ltn12.html http://lua-users.org/wiki/FiltersSourcesAndSinks
+
+  -- handle response header
+  local line, code, error = linesrc()
+  if not line then
+    docker_socket:close()
+    return {code = code, error = error}
+  end
+  local response = {code = 0, headers = {}, body = {}}
+
+  local p, code, msg = line:match("^([%w./]+) ([0-9]+) (.*)")
+  response.protocol = p
+  response.code = tonumber(code)
+  response.message = msg
+  line = linesrc()
+  while line and line ~= "" do
+    local key, val = line:match("^([%w-]+)%s?:%s?(.*)")
+    if key and key ~= "Status" then
+      if type(response.headers[key]) == "string" then
+        response.headers[key] = {response.headers[key], val}
+      elseif type(response.headers[key]) == "table" then
+        response.headers[key][#response.headers[key] + 1] = val
+      else
+        response.headers[key] = val
+      end
+    end
+    line = linesrc()
+  end
+  -- handle response body
+  local body_buffer = linesrc(true)
+  if response.headers["Transfer-Encoding"] == "chunked" then
+    local source = chunksource(docker_socket, body_buffer)
+    code, error = ltn12.pump.all(source, (ltn12.sink.table(response.body))) and response.code or 555
+    response.code = code
+    response.error = error
+  else
+    response.body = json.decode(body_buffer)
+  end
   docker_socket:close()
-  return data
+  return response
 end
 
 local send_http_require = function(options, method, path, operation, name_or_id, query_parameters, req_parameters)
@@ -72,10 +176,7 @@ local send_http_require = function(options, method, path, operation, name_or_id,
   if type(req_parameters) == "table" then
     req_options.conetnt = req_parameters
   end
-  local res = send_http_socket(req_options.socket_path, gen_http_req(req_options))
-  local code = string.match(res, "^HTTP/1.1 (.-) .-\r\n")
-  local res_json_str = string.match(res, "[%[{].+[%]}]")
-  return code, json.decode(res_json_str)
+  return send_http_socket(req_options.socket_path, gen_http_req(req_options))
 end
 
 local gen_api = function(_table, http_method, api_group, api_action)
@@ -101,6 +202,16 @@ local gen_api = function(_table, http_method, api_group, api_action)
         query_parameters.name = query_parameters.name or name_or_id
         name_or_id = nil
       end
+    elseif api_action == "logs" then
+      local body_buffer = {}
+      local response = send_http_require(self.options, http_method, api_group, _api_action, name_or_id, query_parameters, req_parameters)
+      if response.code >= 200 and response.code < 300 then
+        for i,v in ipairs(response.body) do
+          body_buffer[#body_buffer+1] = docker_stream_filter(response.body[i])
+        end
+        response.body = body_buffer
+      end
+      return response
     end
     return send_http_require(self.options, http_method, api_group, _api_action, name_or_id, query_parameters, req_parameters)
   end
